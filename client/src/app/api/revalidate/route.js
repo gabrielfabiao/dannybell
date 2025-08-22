bb68b340ec92f7e61c34eeb399fa1ae6718f4442
Gabriel Rodrigues <gabrielfilipedic@gmail.com>
2025-08-22 12:35:15 +0100

    revalidation fixes

--- client/src/app/api/revalidate/route.js
+++ client/src/app/api/revalidate/route.js
@@ -1,24 +1,21 @@
+import { revalidatePath } from 'next/cache';
+
 export async function GET(req) {
   const { searchParams } = new URL(req.url);
 
   const secret = searchParams.get("secret");
   if (secret !== process.env.REVALIDATE_SECRET) {
-    return new Response(JSON.stringify({ message: "Invalid token" }), {
-      status: 401,
-    });
+    return new Response(JSON.stringify({ message: "Invalid token" }), { status: 401 });
   }
 
   try {
     const path = searchParams.get("path") || "/";
 
-    await res.revalidate(path);
+    // Revalidate the specific path
+    revalidatePath(path);
 
-    return new Response(JSON.stringify({ revalidated: true, path }), {
-      status: 200,
-    });
+    return new Response(JSON.stringify({ revalidated: true, path }), { status: 200 });
   } catch (err) {
-    return new Response(JSON.stringify({ message: "Error revalidating", error: err }), {
-      status: 500,
-    });
+    return new Response(JSON.stringify({ message: "Error revalidating", error: err }), { status: 500 });
   }
 }
